83254cff14c05fb01b5199798abcfbbe8d7a8c16
liangjunweiGIT <liangjunweiemail@163.com>
2018-07-21 00:20:34 +0800

    测试sql文件

--- src/main/java/com/ibatis/com/ljw/ibatis/common/pojo/InitialResultClass.java
+++ src/main/java/com/ibatis/com/ljw/ibatis/common/pojo/InitialResultClass.java
@@ -1,9 +1,8 @@
 package com.ibatis.com.ljw.ibatis.common.pojo;
 
 /**
- * @Description
+ * @Description 做resultClass的初始值
  * @Author Create by junwei.liang on 2018/7/20
  */
 public class InitialResultClass {
-    private int i;
 }
